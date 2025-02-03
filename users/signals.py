0a83ce30e9a450e7369ceef5f57ffda8788f837b
matheus <matheuseduardo3004@gmail.com>
2025-02-03 11:18:01 -0300

    att develop config

--- users/signals.py
+++ users/signals.py
@@ -1,15 +1,19 @@
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 from .models import User
-from decouple import config  # ou from dotenv import load_dotenv, dotenv_values
+from dotenv import load_dotenv
+import os
+
+# Carrega as variáveis do arquivo .env
+load_dotenv()
 
 @receiver(post_migrate)
 def create_default_superuser(sender, **kwargs):
     """Cria um superusuário padrão se ele ainda não existir"""
-    if not User.objects.filter(username=config('DEFAULT_USER_USERNAME')).exists():
+    if not User.objects.filter(username=os.getenv('DEFAULT_USER_USERNAME')).exists():
         User.objects.create_superuser(
-            username=config('DEFAULT_USER_USERNAME'),
-            email=config('DEFAULT_USER_EMAIL'),
-            password=config('DEFAULT_USER_PASSWORD')
+            username=os.getenv('DEFAULT_USER_USERNAME'),
+            email=os.getenv('DEFAULT_USER_EMAIL'),
+            password=os.getenv('DEFAULT_USER_PASSWORD')
         )
         print("Superusuário padrão criado com sucesso!")
